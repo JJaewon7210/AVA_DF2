7db57e181ac958ff30f665667f02a4eb34fc8456
JJaewon <jjw012754@gmail.com>
2023-08-16 15:03:00 +0900

    Train: Mosaic On

--- datasets/yolo_datasets.py
+++ datasets/yolo_datasets.py
@@ -326,7 +326,8 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
         self.hyp = hyp
         self.image_weights = image_weights
         self.rect = False if image_weights else rect
-        self.mosaic = self.augment and not self.rect  # load 4 images at a time into a mosaic (only during training)
+        # self.mosaic = self.augment and not self.rect  # load 4 images at a time into a mosaic (only during training)
+        self.mosaic = False
         self.mosaic_border = [-img_size // 2, -img_size // 2]
         self.stride = stride
         self.path = path        
