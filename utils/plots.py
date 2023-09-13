42e89902c3a6abe562f7a59ba627a654f3e20749
JJaewon <jjw012754@gmail.com>
2023-09-13 13:38:22 +0900

    3d module을 빼서 2D로 df만 학습할 때 사용

--- utils/plots.py
+++ utils/plots.py
@@ -147,7 +147,7 @@ def un_normalized_images(images, mean=None, std=None, convert_cv2=True):
 
     return images
 
-def plot_batch_image_from_preds(frames, preds, save_path='batch_image.jpg', labelmap=None, reshape_size=(224,224)):
+def plot_batch_image_from_preds(frames, preds, save_path='batch_image.jpg', labelmap=None, reshape_size=(224,224), norm_input=True):
     if isinstance(frames, torch.Tensor):
         frames = frames.detach().cpu().float().numpy()
     if isinstance(preds, torch.Tensor):
@@ -161,10 +161,17 @@ def plot_batch_image_from_preds(frames, preds, save_path='batch_image.jpg', labe
         pred = preds[i]
 
         for j, dets in enumerate(pred):
-            x1 = int(dets[0] * frame.shape[1])
-            y1 = int(dets[1] * frame.shape[0])
-            x2 = int(dets[2] * frame.shape[1])
-            y2 = int(dets[3] * frame.shape[0])
+            if norm_input:
+                x1 = int(dets[0] * frame.shape[1])
+                y1 = int(dets[1] * frame.shape[0])
+                x2 = int(dets[2] * frame.shape[1])
+                y2 = int(dets[3] * frame.shape[0])
+            else:
+                x1 = int(dets[0])
+                y1 = int(dets[1])
+                x2 = int(dets[2])
+                y2 = int(dets[3])
+                
             cls_ind = int(dets[5])
             random_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
             cv2.rectangle(frame, (x1, y1), (x2, y2), random_color, 2)
